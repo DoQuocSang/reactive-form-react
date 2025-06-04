b7b7191aaffdf601f869a6d8cba96a4f3dbc68a1
Do Quoc Sang <quocsang316@gmail.com>
2025-06-04 22:32:16 +0700

    feat: add button to view demo form

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,12 +1,18 @@
-import { CircleXIcon } from "lucide-react";
+import { CircleXIcon, Pointer } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
   return (
-    <div className="w-full h-screen px-6 flex flex-col justify-center items-center">
-      <div className="text-red-500 flex flex-col gap-2 items-center bg-white rounded-lg p-10 shadow-md">
+    <div className="w-full h-screen px-6 flex flex-col gap-4 justify-center items-center">
+      {/* <div className="text-red-500 flex flex-col gap-2 items-center bg-white rounded-lg p-10 shadow-md">
         <CircleXIcon size={50} />
         <h1 className="text-4xl font-medium">Page not found</h1>
-      </div>
+      </div> */}
+      <Link to={"/product/P001"} className="flex items-center gap-2 font-medium text-slate-700 hover:bg-teal-500 hover:text-white transition duration-150 ease-in-out rounded-lg px-6 py-4 bg-white shadow-md">
+        <Pointer size={20} />
+        Click here to view Product Demo
+      </Link>
+      <p className="text-sm text-slate-700">😆 I haven't completed all the features yet. Hope you don't mind! 😆</p>
     </div>
   );
 }
